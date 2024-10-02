e937c9a7f109ff0319a6734cac7bc75c8d78b5d5
UdaraWickramarathne <bimsaraudara25@gmail.com>
2024-10-02 15:21:43 +0530

    stylesheet add

--- booking/tour-booking.php
+++ booking/tour-booking.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tour Booking Page</title>
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/general.css">
 </head>
 <body>
 
